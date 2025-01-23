d4dfafe2cbd2dbe80616acf062205778908cd283
elsankary-02 <githup800900@gmail.com>
2025-01-23 22:27:31 +0200

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:coffee_shop/core/router/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CoffeeShop());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CoffeeShop extends StatelessWidget {
+  const CoffeeShop({super.key});
 
   @override
   Widget build(BuildContext context) {
